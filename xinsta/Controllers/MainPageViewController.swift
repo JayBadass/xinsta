a5d3893cab08da323df910214b3b02cc3c3072da
JayBadass <jaybadass@naver.com>
2023-08-14 18:09:41 +0900

    [FEAT] : ⚙️ tableView added

--- xinsta/Controllers/MainPageViewController.swift
+++ xinsta/Controllers/MainPageViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    let label = ["1","2","3","4","5"]
+    let img = ["suit.diamond", "suit.heart", "suit.club", "suit.spade", "hexagon"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return label.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
+        
+        cell.postImage.image = UIImage(systemName: img[indexPath.row])
+        cell.postText.text = label[indexPath.row]
+        
+        return cell
+    }
+    
+    
 }
 
