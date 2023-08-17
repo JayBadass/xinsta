c8c6b6665aaf4f8debf283a44e951e4fc0469ab3
user2rum <nauu400@naver.com>
2023-08-18 02:25:26 +0900

    [FEAT] : ⚙️ searchBar, collectionView 구현

--- xinsta/Controllers/SearchViewController.swift
+++ xinsta/Controllers/SearchViewController.swift
@@ -7,14 +7,142 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var searchController: UISearchBar!
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    // 검색어 배열 선언
+    var searchResults: [String] = ["heizeheize", "silver_rain.__", "eungoo_kwon", "bambi__jeju"]
+    
+    // 이미지 배열 선언
+    var images: [UIImage] = [
+        UIImage(named: "search1")!,
+        UIImage(named: "search2")!,
+        UIImage(named: "search3")!,
+        UIImage(named: "search4")!,
+        UIImage(named: "search5")!,
+        UIImage(named: "search6")!
+    ]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //searchController 딜리게이트 선언
+        searchController.delegate = self
+        searchController.placeholder = "검색"
+        //서치바 cancel 텍스트 수정
+        UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).title = "취소"
+        
+        //tableview 딜리게이트 선언
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        let nibb = UINib(nibName: "ResultCell", bundle: nil)
+        myTableView.register(nibb, forCellReuseIdentifier: "ResultCell")
+        
+        //collectionview 딜리게이트 선언
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        myCollectionView.register(nib, forCellWithReuseIdentifier: "CollectionViewCell")
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
 
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.collectionViewCell.image = images[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let image = images[indexPath.row]
+        let collectionViewWidth = collectionView.bounds.width
+        let cellItemForRow: CGFloat = 3
+        let minimumSpacing: CGFloat = 2
+        let width = (collectionViewWidth - (cellItemForRow - 1) * minimumSpacing) / cellItemForRow
+                
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1  // 세로 간격
+    }
 
-   
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1  // 가로 간격
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)  // 여백 설정
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            myCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        myCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("검색창이 눌렸습니다.")
+        searchBar.showsCancelButton = true
+        
+        
+        myTableView.isHidden = false
+        myCollectionView.isHidden = true
+        myTableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        myTableView.reloadData()  // tableView를 새로고침
+        }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        
+        myTableView.isHidden = true
+        myCollectionView.isHidden = false
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath)
+        cell.textLabel?.text = searchResults[indexPath.row]
+        return cell
+    }
+    
+    
 }
