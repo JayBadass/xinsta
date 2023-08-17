074dc341d9b5d2d3b676809dd1df191cb12eab48
LeeJaeheee <sns3434@gmail.com>
2023-08-18 01:47:12 +0900

    [FEAT] : ⚙️ edit profile 기능 구현
    profile page를 reload하는 로직 필요

--- xinsta/Controllers/EditProfilePageViewController.swift
+++ xinsta/Controllers/EditProfilePageViewController.swift
@@ -7,24 +7,74 @@
 
 import UIKit
 
-class EditProfilePageViewController: UIViewController {
-
+class EditProfilePageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBOutlet weak var profileImageView: UIImageView!
-    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var bioTextField: UITextField!
+    
+    var myProfile: User?
+    var selectedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
     }
     
     func setupUI() {
+        guard let _myProfile = users.first(where: {$0.username == myInfo?.username}) else { return }
+        myProfile = _myProfile
+        
+        profileImageView.image = _myProfile.profilePhoto
         profileImageView.circleImage = true
+        
+        firstNameTextField.text = _myProfile.name.first
+        lastNameTextField.text = _myProfile.name.last
+        userNameTextField.text = _myProfile.username
+        bioTextField.text = _myProfile.bio
+    }
+    
+    private func resetUserTextField() {
+        userNameTextField.text = ""
+        userNameTextField.isUserInteractionEnabled = true
     }
     
     @IBAction func doneButtonTapped(_ sender: UIButton) {
+        guard let newUsername = userNameTextField.text else { return }
+        
+        if newUsername != "", isValidUsername(newUsername) {
+            if let newImage = selectedImage {
+                profileImageView.image = newImage
+                myProfile?.profilePhoto = newImage
+            }
+            myProfile?.name = (firstNameTextField.text ?? "", lastNameTextField.text ?? "")
+            myProfile?.username = newUsername
+            myProfile?.bio = bioTextField.text!
+            
+            if let index = users.firstIndex(where: { $0.username == myInfo?.username }) {
+                users[index] = myProfile!
+            }
+            myInfo = myProfile
+            
+            dismiss(animated: true)
+        } else {
+            showInvalidUsernameAlert()
+            resetUserTextField()
+        }
+    }
+    
+    func isValidUsername(_ username: String) -> Bool {
+        return !users.contains(where: {$0.username != myProfile?.username && $0.username == username})
+    }
+
+    func showInvalidUsernameAlert() {
+        let alert = UIAlertController(title: "Invalid Username", message: "The username is already taken or does not meet the required conditions.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
     
     @IBAction func cancelButtonTapped(_ sender: UIButton) {
@@ -32,6 +82,22 @@ class EditProfilePageViewController: UIViewController {
     }
     
     @IBAction func changePhotoButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            selectedImage = pickedImage
+            profileImageView.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
     
 }
--- xinsta/Controllers/ProfilePageViewController.swift
+++ xinsta/Controllers/ProfilePageViewController.swift
@@ -19,7 +19,6 @@ class ProfilePageViewController: UIViewController {
     @IBOutlet weak var editProfileButton: UIButton!
     @IBOutlet weak var postsCollectionView: UICollectionView!
     
-    var myProfile: User?
     var isExpanded = false
     
     override func viewDidLoad() {
@@ -29,26 +28,25 @@ class ProfilePageViewController: UIViewController {
     }
     
     func setupUI() {
-        guard let _myProfile = users.first(where: {$0.username == myInfo?.username}) else { return }
-        myProfile = _myProfile
+        guard let myProfile = users.first(where: {$0.username == myInfo?.username}) else { return }
         
-        profilePageNavigationItem.title = myProfile!.username
+        profilePageNavigationItem.title = myProfile.username
         
-        profileImageView.image = myProfile!.profilePhoto
+        profileImageView.image = myProfile.profilePhoto
         profileImageView.circleImage = true
         
-        postsLabel.text = ("\(myProfile!.counts.posts)")
-        followersLabel.text = ("\(myProfile!.counts.followers)")
-        followingLabel.text = ("\(myProfile!.counts.following)")
+        postsLabel.text = ("\(myProfile.counts.posts)")
+        followersLabel.text = ("\(myProfile.counts.followers)")
+        followingLabel.text = ("\(myProfile.counts.following)")
         
-        usernameLabel.text = myProfile!.username
+        usernameLabel.text = myProfile.username
         
         editProfileButton.layer.cornerRadius = 5
         editProfileButton.layer.borderWidth = 0.5
         editProfileButton.layer.borderColor = UIColor.lightGray.cgColor
         
-        //bioLabel.text = myProfile!.bio
-        bioLabel.text = "일월과 우리의 위하여 얼마나 싹이 예가 능히 피다. 풍부하게 이상 바이며, 그것은 구하지 역사를 미인을 방황하여도, 하는 있다. 웅대한 이것은 가치를 피가 온갖 돋고, 옷을 그리하였는가? 따뜻한 끓는 곧 속에서 그들에게 얼마나 피고 그들에게 싸인 보라. 끓는 따뜻한 뭇 우리 소담스러운 뿐이다. 이상의 구하지 원질이 희망의 우리 끓는다. 새가 같은 고동을 있는 것이다. 이상은 유소년에게서 살 인간이 튼튼하며, 봄바람이다. 같이, 청춘을 미인을 얼마나 쓸쓸하랴? 심장은 우리는 커다란 고동을 동력은 이것은 것이다."
+        bioLabel.text = myProfile.bio
+
         updateLabelLayout(bioLabel)
     }
     
@@ -82,11 +80,6 @@ class ProfilePageViewController: UIViewController {
         }
         isExpanded.toggle()
         view.layoutIfNeeded()
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let editProfileVC = segue.destination as! EditProfilePageViewController
-        //editProfileVC.변수 = 데이터 전달
     }
 
 }
