8a6e88ec92431e99041dede116587a32a7632d81
LeeJaeheee <sns3434@gmail.com>
2023-08-18 21:54:53 +0900

    [REFACTOR] : ♻️ 데이터 모델 리팩토링

--- xinsta/Models/Models.swift
+++ xinsta/Models/Models.swift
@@ -24,17 +24,9 @@ struct UserCount {
     var posts: Int
 }
 
-public enum UserPostType: String {
-    case photo = "Photo"
-    case video = "Video"
-}
-
 public struct UserPost {
-    let identifier: String
-    //let postType: UserPostType
-    //    let thumbnailImage: URL
+    let id = UUID()
     let thumbnailImage: UIImage?
-    //let postURL: URL // either video URL or full resolution photo
     let caption: String
     var likeCount: [PostLike] = []
     var comments: [PostComment] = []
@@ -44,7 +36,7 @@ public struct UserPost {
 
 struct PostLike {
     let username: String
-    let postIdentifier: String
+    let postId: UUID
 }
 
 struct PostComment {
@@ -70,33 +62,6 @@ var posts: [UserPost] = []
 var myInfo: User?
 
 func createDummyData() {
-    // User Dummy Data
-    for i in 1...5 {
-        let user = User(username: "user\(i)",
-                        bio: "User \(i) bio",
-                        name: (first: "First\(i)", last: "Last\(i)"),
-                        profilePhoto: UIImage(systemName: "person.circle.fill")!,
-                        counts: UserCount(followers: i*100, following: i*50, posts: i*10),
-                        joinDate: Date(), password: "user\(i)")
-        users.append(user)
-    }
-    
-    // User Post Dummy Data
-    for i in 1...100 {
-        let postUser = users[i % 5]
-        let post = UserPost(identifier: "post\(i)",
-                            //postType: i % 2 == 0 ? .photo : .video,
-                            //thumbnailImage: URL(string: "https://example.com/post\(i)_thumb.jpg")!,
-                            thumbnailImage: UIImage(named: "panda"),
-                            //postURL: i % 2 == 0 ? URL(string: "https://example.com/post\(i).jpg")! : URL(string: "https://example.com/post\(i)_video.mp4")!,
-                            caption: "Caption for post \(i)",
-                            likeCount: (1...5).map { PostLike(username: "user\($0)", postIdentifier: "post\(postUser.username)_\(i)") },
-                            comments: (1...5).map { PostComment(
-                                username: "user\($0)",
-                                text: "Comment \($0) for post \(postUser.username)_\(i)"
-                            )},
-                            owner: postUser)
-        posts.append(post)
-    }
+
 }
 
